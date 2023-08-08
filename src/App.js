653e2811e3ae1a56cd7e9999dc4d11b10a2dc868
anheejeong <eyrt6973@naver.com>
2023-08-09 00:53:48 +0900

    make router page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
 import classes from './App.module.css';
 import Home from "./components/Home/Home";
+import Search from "./components/Search/Search";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <React.Fragment>
-      <Home />
-    </React.Fragment>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/search' element={<Search />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Search/Search.js
@@ -0,0 +1,10 @@
+import React from "react";
+import classes from './Search.module.css';
+
+const Search = props => {
+    return (
+        <p>Search Page</p>
+    );
+}
+
+export default Search;
